4968388c4c84aedd5df95a73764324d184c7e553
Florencia Daniela Garcia <dgarcia@geding.com.ar>
2026-08-25 12:20:45 -0300

    Se agregaron los nuevos proyectos del cv

--- app/nuestro-trabajo/page.tsx
+++ app/nuestro-trabajo/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { site } from "@/lib/data/site";
 import { projects, projectCount } from "@/lib/data/projects";
 import NuestroTrabajoExplorer from "@/components/nuestro-trabajo/NuestroTrabajoExplorer";
 import BusDivider from "@/components/ui/BusDivider";
